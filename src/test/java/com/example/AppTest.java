f9085155eaf9fb7a19c23f152f128f0cad8ed707
newbieking <9543679+newbie-king@user.noreply.gitee.com>
2023-10-15 23:29:37 +0800

    serializer

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -2,19 +2,28 @@ package com.example;
 
 import static org.junit.Assert.assertTrue;
 
+import java.sql.Date;
+import java.time.LocalDate;
+
 import org.junit.Test;
+
+import com.example.pojo.Address;
+import com.example.pojo.Customer;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() throws Exception{
+        Customer customer = new Customer(1L, "jack", "15122199876",
+                false, LocalDate.now(), Date.valueOf(LocalDate.now()),
+                new Address(123L, "lalala apartment", new String[] { "a", null, "c" }),
+                22);
+        String json = JsonObject.toJson(customer, Customer.class);
+        System.out.println(json);
     }
 }
